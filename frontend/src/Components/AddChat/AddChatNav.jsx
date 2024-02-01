6d8747147b8f66c000522e0336249c4644da52a6
Kuhu Patel <kuhupatel15@gmail.com>
2024-02-01 21:53:16 +0530

    chat ui

---
+++ frontend/src/Components/AddChat/AddChatNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddChatNav = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AddChatNav
--- frontend/src/Components/Home_Page.jsx
+++ frontend/src/Components/Home_Page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home_Page = () => {
   return (
-    <div>Homepage</div>
+    <div className='flex h-[100vh]'>
+      <div className='w-[40vw] h-full bg-[#37393F]'></div>
+      <div className='w-[60vw] h-full'></div>
+    </div>
   )
 }
 
