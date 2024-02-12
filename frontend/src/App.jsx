81e857295afcb9ae1bf76d34bd4bb65741222c57
Kuhu Patel <kuhupatel15@gmail.com>
2024-02-12 23:08:53 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import Signup from './components/Signup.jsx';
-import Login from './components/Login.jsx';
-import Verify_otp from './components/Verify_otp.jsx';
-import Home_page from './components/Home_Page.jsx';
-import Forgot from './components/Forgot_Password.jsx';
-import Reset from './components/Reset_Password.jsx';
+import Signup from './Components/Signup.jsx';
+import Login from './Components/Login.jsx';
+import Verify_otp from './Components/Verify_otp.jsx';
+import Home_page from './Components/Home_Page.jsx';
+import Forgot from './Components/Forgot_Password.jsx';
+import Reset from './Components/Reset_Password.jsx';
 import { Toaster } from 'react-hot-toast';
 import Protected from './Layouts/Protected.jsx';
 function App() {
