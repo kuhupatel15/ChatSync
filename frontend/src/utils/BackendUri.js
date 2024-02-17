ec46279b5e4c72acc25e68ba7bdc14fe5ca2e5e1
jatin1793 <104287949+jatin1793@users.noreply.github.com>
2024-02-17 13:04:21 +0530

    Update BackendUri.js

--- frontend/src/utils/BackendUri.js
+++ frontend/src/utils/BackendUri.js
@@ -1,2 +1,3 @@
 // export const backendUri = "https://chat-sync-backend-jpn0.onrender.com";
-export const backendUri = "http://localhost:3000";
+// export const backendUri = "http://localhost:3000";
+export const backendUri = "https://chat-sync-backend-m76a.onrender.com"
