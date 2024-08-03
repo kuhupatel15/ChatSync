55b5000bf1f98b42c2c50cdb63bf7e11628ba9bf
jatin1793 <104287949+jatin1793@users.noreply.github.com>
2024-08-03 18:15:42 +0530

    deploy err

--- backend/index.js
+++ backend/index.js
@@ -77,7 +77,7 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     // origin: "http://localhost:5173",
-    origin: env_config.frontend_url
+    origin: "http://localhost:3000",
   },
 });
 
