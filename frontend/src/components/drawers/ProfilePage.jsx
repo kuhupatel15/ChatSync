4a954b51e62a003b3493b72a4933851190454e8a
jatin1793 <104287949+jatin1793@users.noreply.github.com>
2024-08-04 19:45:36 +0530

    drawers ui and img preview

--- frontend/src/components/drawers/ProfilePage.jsx
+++ frontend/src/components/drawers/ProfilePage.jsx
@@ -1,8 +1,10 @@
 import React, { useRef, useState } from 'react'
 import { Button, Avatar } from "@nextui-org/react";
 import { MdDone } from "react-icons/md";
+import { MdOutlineDone } from "react-icons/md";
 import { RxCross1 } from "react-icons/rx";
-import { MdOutlineModeEdit } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 import { CameraIcon } from '@radix-ui/react-icons'
 import { useDispatch } from 'react-redux';
 import { login } from '../../../store/reducers/UserSlice.js';
@@ -31,55 +33,57 @@ const ProfilePage = ({ isProfileOpen, setIsProfileOpen, user }) => {
   };
 
   return (
-    <div
-      className='bg-white'
-      style={{ position: "absolute", height: "100vh", transform: `translateX(${isProfileOpen ? "0%" : "-100%"})`, width: "100%", transition: "ease-in-out 0.5s", top: "0%", zIndex: "99999999" }}
-    >
-      <div className='h-[15vh] bg-sec text-white flex justify-between  items-center px-4 text-xl'>
-        <span>Profile</span>
-        <RxCross1 className='hover:cursor-pointer' onClick={() => setIsProfileOpen(false)} />
+    <div className='bg-pri' style={{ position: "absolute", height: "100%", transform: `translateX(${isProfileOpen ? "0%" : "-100%"})`, width: "100%", transition: "ease-in-out 0.5s", top: "0%", zIndex: "99999999" }} >
+      <div className='bg-sec h-[15%] flex justify-between items-center px-6'>
+        <span className='text-white font-bold text-2xl tracking-wider'>Profile</span>
+        <MdKeyboardDoubleArrowLeft className='hover:cursor-pointer text-white text-xl' onClick={() => setIsProfileOpen(false)} />
       </div>
 
-      <div className='h-[120vh]'>
-        <div className="w-full flex flex-col justify-center text-white text-[1.5vw] bg-pri items-center py-6 px-10 ">
-          <div className="w-[15vw] h-[15vw] relative">
-            <Avatar src={user?.profileImg} className="w-full h-full absolute top-0 left-0 opacity-70" />
-
-            <div className="w-full h-full flex flex-col items-center justify-center absolute top-0 left-0 rounded-full opacity-0 bg-black hover:opacity-50 hover: hover:cursor-pointer" onClick={() => inputRef.current.click()}>
-              <input type="file" name='file' onChange={handleUpload} ref={inputRef} className='hidden' />
-              <CameraIcon />
-              <span className='text-sm mt-2'>CHANGE</span>
-              <span className='text-sm'>PROFILE PHOTO</span>
-            </div>
+      <div className="w-full flex flex-col justify-center text-white text-[1.5vw] bg-pri items-center py-6 px-10 ">
+        <div className="relative h-[30vh] w-[30vh]">
+          <Avatar src={user?.profileImg} className="w-full h-full absolute top-0 left-0 opacity-70" />
+          <div
+            className="w-full h-full flex flex-col items-center justify-center absolute top-0 left-0 rounded-full opacity-0 bg-black hover:opacity-50 hover: hover:cursor-pointer"
+            onClick={() => inputRef.current.click()}
+          >
+            <input type="file" name='file' onChange={handleUpload} ref={inputRef} className='hidden' />
+            <CameraIcon />
+            <span className='text-sm mt-2'>CHANGE</span>
+            <span className='text-sm'>PROFILE PHOTO</span>
           </div>
+        </div>
 
-          <div className='flex items-center h-[7vh] justify-center gap-4 mt-4' >
+        <div className='flex flex-col gap-2 mt-12'>
+          <span className='text-md font-light text-gray-400'>Your username</span>
+
+          <div className='flex items-center gap-6 mb-2'>
             <h6
               inputMode='text'
               onInput={(e) => setUsername(e.currentTarget.textContent)}
               contentEditable={edit}
-              className={edit ? 'grpname' : "min-w-[8vw] duration-100 ease-in-out"}
+              className={edit ? 'grpname tracking-wider' : "text-lg tracking-wider duration-100 ease-in-out"}
             >
               {user?.userName}
             </h6>
-
-            {
-              edit
-              &&
-              <Button
-                isIconOnly
-                onClick={() => rename(user._id, username)}
-                className='mt-2 bg-gradient-to-br from-purple-500  to-cyan-500'
-                variant="faded"
-                aria-label="Take a photo"
-              >
-                <MdDone />
-              </Button>
-            }
-            
-            <MdOutlineModeEdit onClick={() => setEdit(!edit)}></MdOutlineModeEdit>
+            <div className='flex gap-6 items-center text-blue-500'>
+              {edit ?
+                <>
+                  <MdOutlineDone
+                    className={`${username?.length === 0 ? 'pointer-events-none opacity-20' : 'cursor-pointer'}`}
+                    onClick={() => rename(user._id, username)}
+                  />
+                  <RxCross1 className='cursor-pointer ' onClick={() => setEdit(false)} />
+                </>
+                :
+                <MdEdit className='cursor-pointer' onClick={() => setEdit(true)} />
+              }
+            </div>
           </div>
-          
+
+          <div>
+            <span className='text-sm text-gray-400'>This is your username, which will be visible to the other users.</span>
+          </div>
+
         </div>
       </div>
     </div>
--- frontend/src/components/group-chat/NewGrpDrawer.jsx
+++ frontend/src/components/group-chat/NewGrpDrawer.jsx
@@ -4,6 +4,9 @@ import { Button, CheckboxGroup, Checkbox, User, cn } from "@nextui-org/react";
 import { Get_all_users } from "../../routes/UserRoutes.js"
 import { ArrowRightIcon } from '@radix-ui/react-icons';
 import GrpDetsDrawer from './GrpDetsDrawer.jsx';
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import toast from 'react-hot-toast';
+
 
 const CustomCheckbox = ({ user }) => {
   return (
@@ -50,36 +53,28 @@ const NewGrpDrawer = ({ isGrpOpen, setisGrpOpen }) => {
   }, []);
 
   return (
-    <div className='bg-pri' style={{ position: "absolute", height: "100vh", transform: `translateX(${isGrpOpen ? "0%" : "-100%"})`, width: "100%", transition: "ease-in-out 0.5s", top: "0%", zIndex: "99999999" }}>
-      <div className='flex flex-col h-screen'>
-        <div className='bg-sec p-4 text-white flex justify-between items-center'>
-          <span className='text-lg text-white'>Create Group</span>
-          <RxCross1
-            className='hover:cursor-pointer'
-            onClick={() => {
-              setisGrpOpen(false);
-              setMembers([])
+    <div className='bg-pri' style={{ position: "absolute", height: "100%", transform: `translateX(${isGrpOpen ? "0%" : "-100%"})`, width: "100%", transition: "ease-in-out 0.5s", top: "0%", zIndex: "99999999" }} >
+      <div className='bg-sec h-[15%] flex justify-between items-center px-6'>
+        <span className='text-white font-bold text-2xl tracking-wider'>Create Group</span>
+        <MdKeyboardDoubleArrowLeft className='hover:cursor-pointer text-white text-xl' onClick={() => setisGrpOpen(false)} />
+      </div>
+
+      <div className="w-full h-full flex flex-col px-10 justify-between py-12">
+        <div className='w-full h-[80%] overflow-y-auto'>
+          <CheckboxGroup
+            value={members}
+            onChange={setMembers}
+            classNames={{
+              base: "w-full",
             }}
-          />
+          >
+            {users?.length > 0 ? users.map((user) => (
+              <CustomCheckbox key={user._id} user={user} />
+            )) : <span className='text-lg text-gray-400'> No users found !</span>}
+          </CheckboxGroup>
         </div>
 
-        <div className='px-2 h-full overflow-y-auto'>
-          <div className='h-full pt-8 overflow-y-scroll'>
-            <CheckboxGroup
-              value={members}
-              onChange={setMembers}
-              classNames={{
-                base: "w-full",
-              }}
-            >
-              {users?.length > 0 ? users.map((user) => (
-                <CustomCheckbox key={user._id} user={user} />
-              )) : <span> No users found !</span>}
-            </CheckboxGroup>
-          </div>
-        </div>
-
-        <div className='mt-auto py-2 flex justify-center'>
+        <div className='h-[20%] flex justify-center'>
           {members.length > 1 ? (
             <Button
               onClick={() => setgrpDetsDrawerOpen(!grpDetsDrawerOpen)}
@@ -93,7 +88,7 @@ const NewGrpDrawer = ({ isGrpOpen, setisGrpOpen }) => {
           ) : (
             <Button
               isIconOnly
-              onPress={() => alert('At least 2 members need to be selected to create the group.')}
+              onPress={() => toast.error('At least 2 members need to be selected to create the group.')}
               color="primary"
               variant="faded"
               className='cursor-pointer'
